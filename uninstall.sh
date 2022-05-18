c6af04468e0c4599973895822f2a6e358087276d
proffapt <proffapt@pm.me>
2022-05-18 17:55:29 +0530

    modifying setup.sh & uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -7,14 +7,10 @@ blue=$(tput setaf 4)
 cyan=$(tput setaf 6)
 white=$(tput setaf 7)
 
-## making the code-runner(run) executable
-chmod +x run
-echo "${green}[+]${white} Made the run script executable"
-
 ## adding run executable to bin folder
-echo "${green}[+]${white} Adding run executable to your bin folder"
+echo "${green}[+]${white} Removing run executable from your bin folder"
 echo "${yellow}[~]${white} ${blue}To do that this script will be requiring your user password${green}"
-sudo cp ./run /usr/local/bin/run
+sudo rm /usr/local/bin/run
 
 ## the final print message
-echo "${yellow}[~]${white} Now use ${cyan}run --help${white} to go to the help menu"
+echo "${yellow}[~]${white} Delete this folder to remove the existence of ${cyan}run${white} from your system"
